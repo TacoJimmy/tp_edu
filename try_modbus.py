68d0b8810ad37b3633aaa49c568658b6fb7a5f87
TacoJimmy <t5669016@ntut.org.tw>
2023-03-12 00:17:07 +0800

    Update try_modbus.py

--- try_modbus.py
+++ try_modbus.py
@@ -12,12 +12,8 @@ import struct
 
 
 def int16_pair_to_float(num1, num2):
-    """將兩個int16數值轉換為浮點數"""
-    # 將兩個int16組合成一個int32
     combined_num = (num1 << 16) | num2
-    # 使用 struct.pack() 將int32轉換為bytes
     packed_num = struct.pack('i', combined_num)
-    # 使用 struct.unpack() 將bytes轉換為浮點數
     float_num = struct.unpack('f', packed_num)[0]
     return float_num
 

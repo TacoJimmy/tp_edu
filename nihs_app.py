210da0d21e6abc3a298320d7e5f714ac34cb755e
TacoJimmy <t5669016@ntut.org.tw>
2023-04-08 13:25:46 +0800

    Update nihs_app.py

--- nihs_app.py
+++ nihs_app.py
@@ -17,12 +17,94 @@ from modbus_tk import modbus_rtu
 import threading
 import IAQ_Sensor
 import random
+import power_meter
+
 
 # make check
 TVOC_Limit_Flog = 0
 CO2_Limit_Flog = 0
 CO2_Limit = 1000
 TVOC_Limit = 100
+
+def Send_MainPower():
+    try:
+        PowerFrq_Value = power_meter.Read_PowerFreq()
+        PowerVoltage = power_meter.Read_MainPowerVoltage()
+        PowerCurrnet = power_meter.Read_MainPowerCurrnet()
+        PowerkW = power_meter.Read_MainPowerkW()
+        PowerkVAR = power_meter.Read_MainPowerkVAR()
+        PowerkVAS = power_meter.Read_MainPowerkVAS()
+        #MQTT_Connect()
+        client = mqtt.Client()
+        client.on_connect
+        client.username_pw_set('KMS1AXvpxzHiuEHx3G6t','')
+        print(client.connect('thingsboard.cloud', 1883, 60))
+    
+        payload_iaq = {"PowerFrq":PowerFrq_Value,
+                       "PowerV1":PowerVoltage[0],
+                       "PowerV2":PowerVoltage[1],
+                       "PowerV3":PowerVoltage[2],
+                       "PowerVavg":PowerVoltage[3],
+                       "PowerI1":PowerCurrnet[0],
+                       "PowerI2":PowerCurrnet[1],
+                       "PowerI3":PowerCurrnet[2],
+                       "PowerIavg":PowerCurrnet[3],
+                       "PowerkW1":PowerkW[0],
+                       "PowerkW2":PowerkW[1],
+                       "PowerkW3":PowerkW[2],
+                       "PowerkWavg":PowerkW[3],
+                       "PowerkVAR1":PowerkVAR[0],
+                       "PowerkVAR2":PowerkVAR[1],
+                       "PowerkVAR3":PowerkVAR[2],
+                       "PowerkVARavg":PowerkVAR[3],
+                       "PowerkVAS1":PowerkVAS[0],
+                       "PowerkVAS2":PowerkVAS[1],
+                       "PowerkVAS3":PowerkVAS[2],
+                       "PowerkVASavg":PowerkVAS[3]
+                       }
+        print(json.dumps(payload_iaq))
+        print(client.publish("v1/devices/me/telemetry", json.dumps(payload_iaq)))
+        time.sleep(5)
+    except:
+        power_meter.create_modbus_connection()
+        print ("error")
+
+def Send_SubPower():
+    try:
+        for i in range(4):
+            PowerCurrnet = power_meter.Read_SubPowerCurrnet(i)
+            PowerkW = power_meter.Read_SubPowerkW(i)
+            PowerkVAR = power_meter.Read_SubPowerkVAR(i)
+            PowerkVAS = power_meter.Read_SubPowerkVAS(i)
+            #MQTT_Connect()
+            clientsub = mqtt.Client()
+            clientsub.on_connect
+            clientsub.username_pw_set('q62EdLRp3iUrFHDFeg4i','')
+            print(clientsub.connect('thingsboard.cloud', 1883, 60))
+    
+            payload_iaq = {"SubPowerI1"+str(i):PowerCurrnet[0],
+                       "SubPowerI2"+str(i):PowerCurrnet[1],
+                       "SubPowerI3"+str(i):PowerCurrnet[2],
+                       "SubPowerIavg"+str(i):PowerCurrnet[3],
+                       "SubPowerkW1"+str(i):PowerkW[0],
+                       "SubPowerkW2"+str(i):PowerkW[1],
+                       "SubPowerkW3"+str(i):PowerkW[2],
+                       "SubPowerkWavg"+str(i):PowerkW[3],
+                       "SubPowerkVAR1"+str(i):PowerkVAR[0],
+                       "SubPowerkVAR2"+str(i):PowerkVAR[1],
+                       "SubPowerkVAR3"+str(i):PowerkVAR[2],
+                       "SubPowerkVARavg"+str(i):PowerkVAR[3],
+                       "SubPowerkVAS1"+str(i):PowerkVAS[0],
+                       "SubPowerkVAS2"+str(i):PowerkVAS[1],
+                       "SubPowerkVAS3"+str(i):PowerkVAS[2],
+                       "SubPowerkVASavg"+str(i):PowerkVAS[3]
+                       }
+            print(json.dumps(payload_iaq))
+            print(clientsub.publish("v1/devices/me/telemetry", json.dumps(payload_iaq)))
+            time.sleep(5)
+    except:
+        power_meter.create_modbus_connection()
+        print ("error")
 
 # IAQ over limit send linebot
 def Send_LineBot(IAQ_Cond,IAQ_value):
@@ -156,9 +238,10 @@ def check_Iaq():
     except:
         pass
 
-schedule.every(1).minutes.do(Send_Iaq)
 schedule.every(5).seconds.do(check_Iaq)
-
+schedule.every(1).minutes.do(Send_Iaq)
+schedule.every(1).minutes.do(Send_MainPower)
+schedule.every(1).minutes.do(Send_SubPower)
 
 if __name__ == '__main__':
     
